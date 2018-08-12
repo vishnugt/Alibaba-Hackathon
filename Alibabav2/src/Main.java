f3ac523c7b91d6a624b3028d779bbe01d6c0ed6e
ralphat <ralphat@users.noreply.github.com>
2018-08-12 23:52:36 +0530

    Added interference code

--- Alibabav2/src/Main.java
+++ Alibabav2/src/Main.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -17,7 +18,9 @@ public class Main
     static HashMap<String, String> instanceVsApp;
     static HashMap<String, String> instanceVsMachine;
     static HashMap<String, List<String>> machineVsInstances;
+    static HashMap<String, HashMap<String, Integer>> machineVsappVsAppCount;
     static HashMap<String, HashMap<String, Integer>> appVsAppInterference;
+    static HashMap<String, HashMap<String, Integer>> inverseAppVsAppInterference;
     static HashMap<String, HashMap<String, Integer>> machineVsAllergicApps;
 
     static
@@ -28,7 +31,9 @@ public class Main
             instanceVsApp = new HashMap<>();
             instanceVsMachine = new HashMap<>();
             machineVsInstances = new HashMap<>();
+            machineVsappVsAppCount = new HashMap<>() ;
             appVsAppInterference = new HashMap<>();
+            inverseAppVsAppInterference = new HashMap<>();
     }
 
     static int numConstraints = 200;
@@ -38,7 +43,7 @@ public class Main
     {
         loadMachineResourceFile(new BufferedReader(new FileReader(Utility.machineResourceFile)));
         loadAppResourcesFile(new BufferedReader(new FileReader(Utility.appResourcesFile)));
-        loadappInterferenceFile(new BufferedReader(new FileReader(Utility.appInterferenceFile)));
+        loadAppInterferenceFile(new BufferedReader(new FileReader(Utility.appInterferenceFile)));
         loadInstanceDeployFile(new BufferedReader(new FileReader(Utility.instanceDeployFile)));
 
         int numAssigned = 0;
@@ -68,7 +73,7 @@ public class Main
                             break;
                         }
                     }
-                    if (isAssigned && !checkIfMachineIsAllergic())
+                    if (isAssigned && !checkIfMachineIsAllergic(machine, instance))
                     {
                         numAssigned++;
                         machineVsusedMachineResource.put(machine, updatedMachineUsedResources);
@@ -181,13 +186,15 @@ public class Main
                                     machineVsInstances.put(machineName, new ArrayList<>());
                             }
                             machineVsInstances.get(machineName).add(resource[0]);
-                            // Add allergies to machine
-                            instanceList = machineVsInstances.get(machineName);
-                            for (String instance : instanceList){
-                            String app = instanceVsApp.get(instance);
-                            HashMap<String, Integer> interference = appVsAppInterference.get(app);
-                            machineVsAllergicApps.put(machineName,interference);
-                        }
+                            // add app count to machine
+                            String app = instanceVsApp.get(instanceName);
+                            HashMap<String, Integer> appVsAppCountMap = machineVsappVsAppCount.get(machineName);
+                            if(appVsAppCountMap == null){
+                                appVsAppCountMap = new HashMap<>();
+                                machineVsappVsAppCount.put(machineName, appVsAppCountMap);
+                                appVsAppCountMap.put(app,0);
+                            }
+                            appVsAppCountMap.put(app,appVsAppCountMap.get(app)+1);
                             
                             Double[] appResource = appVsAppResource.get(instanceVsApp.get(instanceName));
                             Double[] machineUsedResource = machineVsusedMachineResource.get(machineName);
@@ -203,7 +210,7 @@ public class Main
     }
     
     @SuppressWarnings("oracle.jdeveloper.java.nested-assignment")
-    public static void loadappInterferenceFile(BufferedReader br) throws IOException {
+    public static void loadAppInterferenceFile(BufferedReader br) throws IOException {
         @SuppressWarnings("oracle.jdeveloper.java.string-constructor")
         String line = new String();
         while ((line = br.readLine()) != null)
@@ -212,15 +219,65 @@ public class Main
             String app1Name = resource[0];
             String app2Name = resource[1];
             Integer app2Count = Integer.parseInt(resource[2]);
-            
-            HashMap<String,Integer> app2 = new HashMap<String,Integer>();
-            app2.put(app2Name, app2Count);
-            appVsAppInterference.put(app1Name, app2);
+
+            HashMap<String, Integer> interferenceToFirstInstance = appVsAppInterference.get(app1Name);
+
+            if(interferenceToFirstInstance == null){
+                interferenceToFirstInstance = new HashMap<String,Integer>();
+                appVsAppInterference.put(app1Name, interferenceToFirstInstance);
+            }
+            interferenceToFirstInstance.put(app2Name, app2Count);
+
+            HashMap<String, Integer> interferenceToSecondInstance = inverseAppVsAppInterference.get(app1Name);
+
+            if(interferenceToSecondInstance == null){
+                interferenceToSecondInstance = new HashMap<String,Integer>();
+                inverseAppVsAppInterference.put(app2Name, interferenceToSecondInstance);
+            }
+            interferenceToSecondInstance.put(app1Name, app2Count);
         }
         br.close();
     }
     
-    public static boolean checkIfMachineIsAllergic(){
+    public static boolean checkIfMachineIsAllergic(String machine, String currentInstance){
+        String app = instanceVsApp.get(currentInstance);
+        int currentInstanceCount = 0;
+        if(machineVsappVsAppCount.get(machine) == null){
+            return false;
+        }
+        currentInstanceCount = machineVsappVsAppCount.get(machine).get(app) == null ? 0 : machineVsappVsAppCount.get(machine).get(app);
+        
+        for(Map.Entry<String,Integer> existingAppVsAppCount : machineVsappVsAppCount.get(machine).entrySet()){
+            //app1, app2, k
+            //app1 count = a1
+            //app2 count = a2
+            // app 1 getting added
+            // if a2>k, can't add app 1
+            
+            String app1 = app;
+            int a1 = currentInstanceCount;
+            String app2 = existingAppVsAppCount.getKey();
+            int a2 = existingAppVsAppCount.getValue();
+            int k = Integer.MAX_VALUE;
+            if(appVsAppInterference.get(app1) != null){
+                k =
+                    appVsAppInterference.get(app1).get(app2) == null ? Integer.MAX_VALUE :
+                    appVsAppInterference.get(app1).get(app2);
+            }
+
+            if(a2>k){
+                return true;
+            }
+            
+            // app2 getting added
+            // a2=k and a1>1, can't be added
+            // or else, can add.
+            
+            if(a1 == k && a2 > 1){
+                return true;
+            }
+   
+        }
         return false;
     }
 }
